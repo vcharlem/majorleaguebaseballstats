a018ac8ab35235851e94ea7bae70039ba4efb295
Vladmr <vcharlem@gmail.com>
2017-05-10 10:23:56 -0400

    clean up application.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,15 +1,3 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, vendor/assets/javascripts,
-// or vendor/assets/javascripts of plugins, if any, can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file.
-//
-// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
-// about supported directives.
-//
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
@@ -19,17 +7,12 @@
 //= require_tree .
 
  $(function() { 	
-  $(".pagination a").on("click", function() { 
-  			$.getScript(this.href);
-    		return false;
-  		});
- 
 
-  $("#players_search input").keyup(
-  	function() {
-    $.get($("#players_search").attr("action"), $("#players_search").serialize(), null, "script");
-    return false;
-  });
+	  $("#players_search input").keyup(
+	  	function() {
+	    $.get($("#players_search").attr("action"), $("#players_search").serialize(), null, "script");
+	    return false;
+	  });
 
   
 }); 
